7e001adc55a050623ef8ffce7cf7b04163536332
pancharasshubham <pancharasshubham@gmail.com>
2025-02-20 22:22:31 +0530

    learn operation in js

---
+++ JS_basic/04_operation.js
@@ -0,0 +1,30 @@
+let value = 3
+
+let negValue = -value;
+console.log(negValue); // -3
+
+let str1 = "hello"
+let str2 = " saalar"
+
+let str3 = str1 + str2;
+
+console.log(str3); // hello saalar
+
+console.log("1" + 2);       // 12
+console.log(1 + "2");       // 12
+console.log("1" + 2 + 2);   // 122
+console.log(1 + 2 + "2");   // 32
+
+// console.log(+true); // 1
+// console.log(+""); // 
+
+let gameCounter = 100;
+
+++gameCounter;
+console.log(gameCounter); // 101
+// read prefix and postfix on js mdn 
+
+let x = 3;
+const y = x++;
+
+console.log(x , y)
