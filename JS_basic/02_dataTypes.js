c1df995ddda750486a84ba807f3e8181bcae8f8b
pancharasshubham <pancharasshubham@gmail.com>
2025-02-20 16:41:54 +0530

    learn datatypes

---
+++ JS_basic/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+alert("hello")
+
+// code redability must be priority 
+
+let name = "saalar"
+
+let age = 35;
+
+let isLoggedIn = false;
+
+//primitive datatypes
+/*  1) number => 2 to power 53, 
+    2) bigInt, 
+    3) string, 
+    4) boolean, 
+    5) null => standalone value, 
+    6) undefined, 
+    7) symbol => unique */ 
+
+//object
+
+console.log(typeof null); // op -> object
+
