9f2401b158d45624b6d3c473e1537820b15f6555
pancharasshubham <pancharasshubham@gmail.com>
2025-02-20 15:01:21 +0530

    learn variables

---
+++ JS_basic/01_Variables.js
@@ -0,0 +1,22 @@
+console.log("hello saalar");
+
+const accountId = 12345
+let accountEmail = "devaratha@gmail.com"
+var accountPassword = "Maa@143"
+
+accountCity = "aasam";
+let accountstate;
+
+
+//accountID - will not change due to const declaration
+
+accountEmail = "rajmanar_ka_salar@gmail.com"
+accountPassword = "Rajmanar@143"
+accountCity = "khansar"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
