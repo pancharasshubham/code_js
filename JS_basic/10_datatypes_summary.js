7cc2b8679ce063e0a186fd0cba78cab55ce45c11
pancharasshubham <pancharasshubham@gmail.com>
2025-02-21 23:51:09 +0530

    summary of datatypes

---
+++ JS_basic/10_datatypes_summary.js
@@ -0,0 +1,33 @@
+// primitive
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// reference (non primitive)
+
+// symbol - datatype
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+//Array, Objects, Functions
+
+//array
+const khansar = ["rajmanar", "deva", "bhargva"]
+
+//object
+let myObj = {
+    name: "devaratha raisar",
+    age: 32,
+}
+
+//funciton
+const myfunction = function(){
+    console.log("khansar ka saalar");
+}
+
+
+// javascript is danyamic type or static type ? 
+
+
+
