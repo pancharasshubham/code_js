6ecd42a85fcc20340dbab1fc0f03c2f5fcc966c2
pancharasshubham <pancharasshubham@gmail.com>
2025-02-25 19:14:47 +0530

    learn switch statement

---
+++ control_flow/02_switch.js
@@ -0,0 +1,20 @@
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January")
+        break;
+    case "feb":
+        console.log("Feburary")
+        break;
+    case "march":
+        console.log("March")
+        break;    
+    case "april":
+        console.log("April")
+        break;    
+
+    default:
+        console.log("Please See Marathi Calander")
+        break;
+}
