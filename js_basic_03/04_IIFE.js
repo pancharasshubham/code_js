58ae1673f3294d21418ee2b6e4c17f878336e31a
pancharasshubham <pancharasshubham@gmail.com>
2025-02-27 20:07:18 +0530

    updated IIFE

--- js_basic_03/04_IIFE.js
+++ js_basic_03/04_IIFE.js
@@ -5,13 +5,17 @@
 // }
 // saalar()
 
+
+// use -> globle scope ke polution se problem hoti hain kai bar to globle scope ke declare variable ke polution ko hatane ke liye.
+
 // way to declare IIFE function
 (function saalar (){
-    // name IIFE
+    // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); // if you don't use this semicolon it will throw error
 
 ( (name) => {
     console.log(`DB connected TO ${name}`);
 })("khanshar")
 
+// what is named IIFE - google it yourself
