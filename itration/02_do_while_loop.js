87c40055d147c2ed44b509fae25989644a58c40b
pancharasshubham <pancharasshubham@gmail.com>
2025-02-28 00:21:39 +0530

    learn while and do-while loop

---
+++ itration/02_do_while_loop.js
@@ -0,0 +1,36 @@
+// ************************ while loop *****************************
+
+let index = 0
+
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myArry = ["rajmannar","deva","rudra","shivmannar"]
+
+let arr = 0
+while (arr < myArry.length) {
+    // console.log(`value is ${myArry[arr]}`);
+    arr = arr + 1
+}
+
+
+
+// *********************** do while loop *********************************
+
+let score = 1
+
+do {
+    // console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+let score1 = 11
+
+do {
+    console.log(`score is ${score1}`);
+    score1++
+} while (score1 <= 10);     // score1 is 10 
