0ae654edd3c8f3324d9e518e3dba03a0e178d96b
pancharasshubham <pancharasshubham@gmail.com>
2025-03-02 22:10:15 +0530

    tree of for_loop

---
+++ itration/01_for_loops/01.2_for_each.js
@@ -0,0 +1,58 @@
+// for-each is higher order array loops
+
+// const coding = ["js","ruby","python","go","swift"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// });
+
+// for-each with arrow function
+
+// coding.forEach( (val) => {
+    // console.log(val);
+// })
+
+// for-each with function name 
+// function printme(item){
+    // console.log(item);
+// }
+
+// coding.forEach(printme)
+
+// coding.forEach( (item, index, arr) => {
+    // console.log(item, index, arr);
+// })
+
+
+// for each on array of object
+const position = [
+    {
+        name : "rudra",
+        designation : "sardar"
+    },
+    {
+        name : "raajmannar",
+        designation : "kapar"
+    },
+    {
+        name : "devaratha",
+        designation : "unknown"
+    }
+]
+
+position.forEach( (key) => {
+    // console.log(key.name, ' :- ', key.designation);
+    // console.log(key);
+});
+
+
+// const coding = ["js","ruby","python","go","swift"]
+
+// const values = coding.forEach((item) => {
+//     // console.log(item);
+//     return item                                             // for each does not return value
+// })
+
+// console.log(values)                                         // undefined
+
+
