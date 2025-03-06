ce9bafd65594e2795b1914380b86e55880d60ca2
pancharasshubham <pancharasshubham@gmail.com>
2025-03-06 21:22:51 +0530

    updated filter in js

--- itration/04_filter.js
+++ itration/04_filter.js
@@ -3,7 +3,9 @@
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const newNums =  myNums.filter( (num) => num > 4 )
-// console.log(newNums);
+// console.log(newNums);            // [ 5, 6, 7, 8, 9, 10]
+
+// example of explicit return 
 
 const myNums1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
@@ -22,4 +24,28 @@ myNums1.forEach( (num) => {
     }
 })
 
-console.log(numbers);
+// console.log(numbers);
+
+// real-life example of loops on array
+
+const salaarData = [
+    { character: "Deva", role: "Warrior", allegiance: "Kaapar Clan", dob: "10-05-1985" },
+  { character: "Varadha", role: "Prince", allegiance: "Khansaar Royalty", dob: "15-08-1987" },
+  { character: "Vardha's Father", role: "King", allegiance: "Khansaar Royalty", dob: "20-07-1960" },
+  { character: "Bharghav", role: "Antagonist", allegiance: "Rival Faction", dob: "30-11-1982" },
+  { character: "Radha Rama", role: "Love Interest", allegiance: "Khansaar Royalty", dob: "12-02-1990" },
+  { character: "Mani", role: "Loyal Friend", allegiance: "Kaapar Clan", dob: "25-06-1986" },
+  { character: "Samrat", role: "War General", allegiance: "Khansaar Army", dob: "18-09-1975" },
+  { character: "Jeeva", role: "Spy", allegiance: "Rival Faction", dob: "05-12-1983" }
+  ];
+
+let allegianceData = salaarData.filter( (clan) => clan.allegiance === "Khansaar Royalty")
+
+// console.log(allegianceData);
+
+const character_DOB = salaarData.filter( (char) => {
+    const year = parseInt(char.dob.split("-")[2]); // Extract year from DOB
+    return year > 1982 && char.allegiance === "Khansaar Royalty";
+})
+
+console.log(character_DOB);
