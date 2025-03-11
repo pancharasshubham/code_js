654d2e1dbe4dc96309730643dfc826c2dabe396a
pancharasshubham <pancharasshubham@gmail.com>
2025-03-11 15:54:02 +0530

    updated script for BMI project

--- project/BMI_Calculator/BMI.js
+++ project/BMI_Calculator/BMI.js
@@ -10,13 +10,26 @@ form.addEventListener('submit', function (e) {
     if( height <= 0  || isNaN(height)) {
         result.innerHTML = `please give a valid height ${height}`;
     } 
-    else if(weight <= 0 || isNaN(weight)) {
+    if(weight <= 0 || isNaN(weight)) {
         result.innerHTML = `please give a valid weight ${weight}`;
     } 
-    else{
-        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+      
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+    
+    let message = "";
+            if (bmi < 18.6) {
+                message = 'you are underweight'
+            }
+            else if(bmi >= 18.6 && bmi <= 24.9){
+                message = 'your weight is normal'
+            }
+            else if ( bmi >= 24.9){
+                message = 'you are over weight'
+            }
 
         //show result
-        result.innerHTML = `<span>${bmi}</span>`;
+        result.innerHTML = `Your BMI is :<span>${bmi}</span> <br> ${message}`;
+        
     }
-});
+);
