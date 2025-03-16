5839bf5b90fdc0955911676ce32d280717d7a67c
pancharasshubham <pancharasshubham@gmail.com>
2025-03-16 23:26:17 +0530

    updated script for GTN project

--- project/guess_the_number/script.js
+++ project/guess_the_number/script.js
@@ -10,9 +10,26 @@ const startOver = document.querySelector('.resultParas')
 const p = document.createElement('p')
 
 let prevGuess = []
-
 let numGuess = 1
 
 let playGame = true
 
+// this to validate enter value is between 1 - 100
+function validateGuess(guess){
+    //
+}
 
+// to check given value is equal to random no/ high / low
+function checkGuess(guess){
+    //
+}
+
+// to clean prev value and update prv value in array and update guess value 
+function displayGuess(guess){
+
+}
+
+// to print message 
+function displayMessage(message){
+    //
+}
