bedd9bd03019ca632defd9b739ddaccaaf8ed818
pancharasshubham <pancharasshubham@gmail.com>
2025-03-20 23:21:27 +0530

    learn about promise in js

---
+++ advance_one/promise.js
@@ -0,0 +1,51 @@
+const promiseOne = new Promise(function (resolve, reject){
+    // do an async task 
+    // DB Calls , cryptography, network 
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+    }, 1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+
+// another way 
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+// creating another promise 
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username : "shubham", email: "pancharasshubham@gmail.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user)
+})
+
+// promise 4
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "root", password: "toor"})
+        } else {
+            reject('Error: something went wrong')
+        }
+    }, 1000)
+})
+
+promiseFour.then()
