71bde45b580e71b0b335c578c668d4390b92fda4
pancharasshubham <pancharasshubham@gmail.com>
2025-03-29 20:22:41 +0530

    learn about new and this keyword

--- classes_and_oop/oop.js
+++ classes_and_oop/oop.js
@@ -16,3 +16,25 @@ const user = {
 //console.log(user.username)
 //console.log(user.getUserDetails());
 // console.log(this);                   / O/P - {}
+
+const promiseOne = new Promise()            // new - constructor function
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);    // 
+
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
+
+// what is instanceof ? - google it 
