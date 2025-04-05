266328d03cda47f315c9c56ed8b1ded9bc5a1235
pancharasshubham <pancharasshubham@gmail.com>
2025-04-05 23:55:17 +0530

    added script file for debounce project

--- project/debounce/chaiaurcode.js
+++ project/debounce/chaiaurcode.js
@@ -7,3 +7,21 @@ function debounce(func, delay) {
     };
   }
   
+// Fetch data from randomuser.me
+async function fetchUserData(query) {
+    if (!query) {
+      document.getElementById('user-card').style.display = 'none';
+      return;
+    }
+  
+    try {
+      const res = await fetch('https://randomuser.me/api/');
+      const data = await res.json();
+      const user = data.results[0];
+  
+      displayUserCard(user);
+    } catch (error) {
+      console.error('API Error:', error);
+    }
+  }
+
